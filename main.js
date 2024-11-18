076ad100ea9dc898af1135ef47f968a63bf3d1d0
MrAMB312 <a.blomarz@gmail.com>
2024-11-17 18:06:52 -0600

    complete phase 1 building simple liker

--- main.js
+++ main.js
@@ -4,7 +4,59 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// set up an event listener to respond to a user action
 
+// submit a request to a mocked-up server
+
+// update the DOM  based on the mock server's response
+
+const likeGlyphs = document.getElementsByClassName("like-glyph");
+const errorModal = document.getElementById("modal");
+
+for (const glyph of likeGlyphs) {
+  glyph.addEventListener('click', function() {
+    mimicServerCall()
+      .then((res) => {
+        if (glyph.classList.contains("activated-heart")) {
+          glyph.textContent = EMPTY_HEART;
+          glyph.classList.remove("activated-heart");
+        } else {
+          glyph.textContent = FULL_HEART;
+          glyph.classList.add("activated-heart");
+        }
+      })
+      .catch((e) => {
+        errorModal.textContent = e;
+        errorModal.classList.remove("hidden");
+        setTimeout((e) => {
+          errorModal.classList.add("hidden");
+        }, 3000)
+      })
+  })
+}
+
+// for (const glyph of likeGlyphs) {
+//   glyph.addEventListener('click', async () => {
+//     try {
+//       const serverCall = await mimicServerCall();
+//       if (serverCall) {
+//         if (glyph.classList.contains("activated-heart")) {
+//           glyph.textContent = EMPTY_HEART;
+//           glyph.classList.remove("activated-heart");
+//         } else {
+//           glyph.textContent = FULL_HEART;
+//           glyph.classList.add("activated-heart");
+//         }
+//       }
+//     } catch(e) {
+//       errorModal.textContent = e;
+//       errorModal.classList.remove("hidden");
+//       setTimeout((e) => {
+//         errorModal.classList.add("hidden");
+//       }, 3000)
+//     }
+//   })
+// }
 
 
 //------------------------------------------------------------------------------
